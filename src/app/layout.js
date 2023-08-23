f705f35d5611b504c2fc08da774851349a53314e
Ervan Ade Wijaya <eawijaya@cpxi-asia.com>
2023-08-23 12:12:43 +0700

    fix api

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,14 @@ import { Inter } from 'next/font/google'
 import parse from 'html-react-parser';
 
 
-export const metadata = {
-  title: 'Skinaro - Beauty Aro Tea Skin Care',
-  description: 'Skinaro - Beauty Aro Tea Skin Care',
-}
+// export const metadata = {
+//   title: 'Skinaro - Beauty Aro Tea Skin Care',
+//   description: 'Skinaro - Beauty Aro Tea Skin Care',
+// }
 
 export default async function RootLayout({ children }) {
   const scriptString = `<script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=DC-10827733&amp;l=dataLayer&amp;cx=c"></script>
           <script async src="https://www.googletagmanager.com/gtag/js?id=DC-10203694"></script>
-          <!-- Global site tag (gtag.js) - Google Marketing Platform -->
           <script async src="https://www.googletagmanager.com/gtag/js?id=DC-10203694"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
@@ -20,9 +19,6 @@ export default async function RootLayout({ children }) {
           
             gtag('config', 'DC-10203694');
           </script>
-          <!-- End of global snippet: Please do not remove -->
-          
-        <!-- Meta Pixel Code -->
         <script>
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -38,14 +34,12 @@ export default async function RootLayout({ children }) {
         <noscript><img height="1" width="1" style="display:none"
         src="https://www.facebook.com/tr?id=755549682423303&ev=PageView&noscript=1"
         /></noscript>
-        <!-- End Meta Pixel Code -->
-
           `;
       const data = await getData()
   return (
     <html lang="en">
       <head>
-        {/* {parse(scriptString)} */}
+        {parse(scriptString)}
         <meta description={data.title} />
         <title>{data.title}</title>
       </head>
@@ -54,7 +48,7 @@ export default async function RootLayout({ children }) {
   )
 }
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1', { next: {revalidate: 36000}})
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
  
